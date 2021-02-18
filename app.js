f94253ed56cb2ea83699686cb15d3e121bf65c3c
Wesley Knuts <wesley@axaro.be>
2021-02-18 10:06:11 +0100

    deel 2

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 80
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/test', (req, res) => {
+    let arr = ['wesley', 'laura', 'brent'];
+    res.send(JSON.stringify(arr));
+})
+
+app.listen(port, () => {
+    console.log(`App Server luistert op poort ${port}`)
+})
